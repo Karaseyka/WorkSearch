ede2adba044df318eebd349db3dc3a98252719c4
Karaseyka <vladchuprina12@mail.ru>
2023-09-17 19:15:55 +0300

    make button

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def welcome_page():
     return render_template("welcome_page.html")
 
 
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
