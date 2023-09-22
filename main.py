e6ac7255761d14994a19873824f8d54c1b4d34fc
Karaseyka <vladchuprina12@mail.ru>
2023-09-22 23:08:34 +0300

    add sql library and registration page

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///workSearch.db"
+db = SQLAlchemy(app)
+
+
+# class User(db.Model):
+#     pass
 
 
 @app.route("/")
