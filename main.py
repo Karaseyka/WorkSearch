d79042f11df368a05bb4784f42aee6aab7125a32
Karaseyka <vladchuprina12@mail.ru>
2023-09-25 22:39:25 +0300

    add User model and db file

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///workSearch.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///worksearch.db"
 db = SQLAlchemy(app)
 
 
-# class User(db.Model):
-#     pass
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    role = db.Column(db.String)
+
+    def __repr__(self):
+        return "<User %r>" % self.id
 
 
 @app.route("/")
@@ -15,8 +22,25 @@ def welcome_page():
     return render_template("welcome_page.html")
 
 
-@app.route("/registration")
-def registration():
+@app.route("/registration", methods=["POST"])
+def registration_post():
+    if request.form['password'] == request.form['passwordSec']:
+        user = User(password=request.form["password"], name=request.form["name"],
+                    email=request.form["email"], role=request.form['radios'])
+        print(user.role)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Технические шоколадки"
+
+    else:
+        return render_template("registration.html")
+
+
+@app.route("/registration", methods=["GET"])
+def registration_get():
     return render_template("registration.html")
 
 
