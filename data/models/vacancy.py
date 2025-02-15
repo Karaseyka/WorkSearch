e795b24ac33dcc56187e4f34e40f87eb08786673
Karaseyka <vladchuprina12@mail.ru>
2025-02-15 14:21:42 +0300

    add instruction

--- data/models/vacancy.py
+++ data/models/vacancy.py
@@ -9,11 +9,11 @@ from data.database.db_session import SqlAlchemyBase
 class Vacancy(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'vacancies'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    owner = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("user.id"))
-    name = sqlalchemy.Column(sqlalchemy.String)
-    description = sqlalchemy.Column(sqlalchemy.String)
-    minimal_age = sqlalchemy.Column(sqlalchemy.String)
-    salary = sqlalchemy.Column(sqlalchemy.Integer)
-    town = sqlalchemy.Column(sqlalchemy.String)
-    address = sqlalchemy.Column(sqlalchemy.String)
+    owner = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("user.id")) # id работодателя
+    name = sqlalchemy.Column(sqlalchemy.String) # название вакансии
+    description = sqlalchemy.Column(sqlalchemy.String) # описание вакансии
+    minimal_age = sqlalchemy.Column(sqlalchemy.String) # минимальный возраст на отклик
+    salary = sqlalchemy.Column(sqlalchemy.Integer) # зарплата
+    town = sqlalchemy.Column(sqlalchemy.String) # город
+    address = sqlalchemy.Column(sqlalchemy.String) # полный адрес
 
